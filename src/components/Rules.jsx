1d60967e74f9713145a82bfef887e492e3fdb076
RuthG7 <152434714+RuthG7@users.noreply.github.com>
2023-12-19 13:51:10 -0600

    update rules

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -5,13 +5,62 @@ export default function Rules() {
   return (
     <div className="landing-page">
     <Header />
-    <main className="container px-6 py-8 bg-gray-900 bg-opacity-50 md:m-8 max-w-4xl">
-      <h1 className="text-white-400 mb-3 text-left font-palanquin text-xl leading-relaxed">Samsung Galaxy Marketing Case Competition</h1>
-      <p className="text-white-400 text-left m-3 font-palanquin text-xl leading-relaxed">
-      NO PURCHASE NECESSARY. OPEN ONLY TO LEGAL RESIDENTS OF THE 50 UNITED STATES AND DISTRICT OF COLUMBIA, 18 OR OVER AS OF DATE OF ENTRY, 
-            WHO ARE COLLEGE/UNIVERSITY STUDENTS CURRENTLY ENROLLED IN AN ACCREDITED COLLEGE OR UNIVERSITY PURSUING AN UNDERGRADUATE OR GRADUATE 
-            DEGREE IN THE 50 UNITED STATES OR THE DISTRICT OF COLUMBIA, AND ARE WILLING TO TRAVEL AT THEIR OWN EXPENSE TO NEW YORK, NY ON DATES 
-            SURROUNDING AUGUST 17, 2023 THROUGH AUGUST 18, 2023.</p>
+    <main className="container px-6 py-8 bg-gray-900 bg-opacity-50 md:m-8 max-w-6xl text-white-400">
+    <h4>Samsung Galaxy Marketing Case Competition</h4>
+      <p>Official Rules</p>
+      <div>
+      <p className='text-sm'>
+        NO PURCHASE NECESSARY. OPEN ONLY TO LEGAL RESIDENTS OF THE 50 UNITED STATES AND DISTRICT OF COLUMBIA, 18 OR OVER AS OF DATE OF ENTRY,
+        WHO ARE COLLEGE/UNIVERSITY STUDENTS CURRENTLY ENROLLED IN AN ACCREDITED COLLEGE OR UNIVERSITY PURSUING AN UNDERGRADUATE OR GRADUATE
+        DEGREE IN THE 50 UNITED STATES OR THE DISTRICT OF COLUMBIA, AND ARE WILLING TO TRAVEL AT THEIR OWN EXPENSE TO NEW YORK, NY ON DATES
+        SURROUNDING AUGUST 17, 2023 THROUGH AUGUST 18, 2023.
+      </p>
+      </div>
+      <ol className='list-decimal list-inside text-left mt-4 space-y-4'>
+    <li>Contest Period: Samsung Galaxy Marketing Case Competition (the “Contest”) begins at 7:00 p.m. Eastern Time (“ET”) on 7/20/23 and ends at 11:59 p.m. ET on July 30, 2023 (the “Contest Period”), which includes the registration period, which begins 7:00 p.m. ET on 7/20/23 and ends at 11:59 p.m. ET July 23, 2023 (“Registration Period”)</li>
+    <li>Eligibility: Open to legal residents of the 50 United States and the District of Columbia, 18 years of age or older as of date of entry, who are college/university students currently enrolled in an accredited college or university pursuing an undergraduate degree or graduate degree in the 50 United States or the District of Columbia, and are willing to travel at their own expense to New York, NY on dates surrounding August 17, 2023 through August 18, 2023 (“Entrants”). Employees and the immediate family members (and those residing in the same household regardless of whether or not they are related) of Samsung Electronics America, Inc. (“Sponsor”), its respective parents, subsidiaries, divisions, affiliates, suppliers, distributors and advertising, promotional and judging agencies and their respective officers, directors, shareholders, members, employees, agents, assigns, and all others associated with the development, evolvement and execution of this Contest (collectively “Sponsor and its Administrators”) are not eligible to participate or win.</li>
+    <li>How To Register and Enter during Round 1: During the Registration Period, you may participate by visiting SamsungMarketingCase.com (the “Site”) and following the onscreen instructions to complete the registration form (“Pre-Registration”). Pre-Registration must be complete by 11:59 p.m. ET on July 23, 2023. Once you've completed the Pre-Registration process, if you indicate that you are 18+ and willing/available to travel to New York on dates surrounding August 17, 2023 - August 18, 2023, you will receive an email within 24 hours with a detailed brief (the “Email Brief”). From there, you will be required to follow the instructions in the Email Brief to share all deliverables requested in the Email Brief via a unique Google Form provided by Sponsor (the “Round 1 Entry”). You will also have the opportunity to send questions to the Sponsor regarding the Round 1 Entry process. Responses to questions received will be sent via email to all Entrants to view on or around July 25, 2023. Round 1 Entry must be submitted by 11:59 p.m. ET on July 30, 2023 to be eligible. Entries must be submitted as a link to Google Slides and may not contain more than five (5) Google Slides.</li>
+<p>Each Round 1 Entry will be judged per the Judging Criteria listed below to determine up to twenty (20) Semi-Finalists and ten (10) Alternates. Limit one (1) Round 1 Entry per Entrant. All Round 1 Entries must be in English. The Round 1 Entry may not contain obscene, lewd or inappropriate images or text, as determined at Sponsor's sole discretion. Any Round 1 Entry not in compliance with the above/Email Brief will be disqualified. All Entries must be the sole, original work of the Entrant, must be submitted by Entrant only (no teams) and Entries may not have won any previous awards or competitions. Entrants may not receive any professional advice, critiques, or assistance in the development of Round 1 Entry. All Entries submitted become the non-exclusive property of the Sponsor and will not be returned. No information regarding Entries, other than as otherwise set forth in the Official Rules, will be disclosed. Each Entrant grants to Sponsor the right to use, publish, and broadcast his/her name, voice, picture, portrait, image and likeness in all media and types of advertising and promotion. Entrants agree that all rights to the Round 1 Entry belong to them and the Round 1 Entry does not infringe, misappropriate or violate the rights of any third party including, but not limited to, copyright, trademark, or other intellectual property rights, rights of publicity or privacy or defamation, or portrayal in a false light. No copyrighted music, trademarks, brands, or other intellectual property not owned by Entrant or Sponsor may be used in Round 1 Entry, other than Samsung. Any Round 1 Entry deemed unsuitable will be disqualified in Sponsor's sole discretion. In the event of a dispute as to the identity of an Entrant, the authorized account holder of the email address used to enter will be deemed to be the Entrant. “Authorized account holder” of an email address is defined as the natural person who is assigned to an email address by an Internet Access provider, online service provider, or other organization (e.g., business, educational institution, etc.) that is responsible for assigning email addresses for the domain associated with the submitted email address. Normal Internet access and usage charges imposed by Entrants' online service will apply.</p>    
+    <li>Semi-Finalist Determination via Judging, Semi-Finalist Notification, Round 2 Semi-Finalist Submissions, and Round 3 Grand Prize Judging: All Round 1 Entries received will be judged by Sponsor-select judges (“Judges”) on or about August 3, 2023 to determine up to twenty (20) Semi-Finalists and ten (10) Alternates, based on the following criteria (“Judging Criteria”):</li>
+    <ol className='custom-list list-inside text-left mt-4 space-y-4 pl-4'>
+  <li>Strategy/Goals: 33%</li>
+  <ol className='custom-list-roman list-inside text-left pl-4'>
+  <li>What is the ad/campaign's objectives?</li>
+  <li>Who is the target audience?</li>
+  <li>What are the insights that shaped the idea?</li>
+  <li>How are they relevant to the brand and audience?</li>
+</ol>
+  <li>Execution (Use of Media, Impact, Creative): 33%</li>
+  <ol className='custom-list-roman list-inside text-left mt-4 pl-4'>
+  <li>What is the creative/strategic solution?</li>
+  <li>What channel(s) are used?</li>
+</ol>
+
+  <li>Results and ROI: 33%</li>
+  <ol className='custom-list-roman list-inside text-left mt-4 pl-4'>
+  <li>Include expected/anticipated results as well as change in awareness and attitudes.</li>
+  <li>Do results map back to the objectives and goals?</li>
+</ol>
+  </ol>
+
+  <p>Any tied Round 1 Entries will be rejudged by the Judges based 100% on Execution to determine the applicable Semi-Finalist/Alternate.</p>
+  <p>Potential Semi-Finalists will be notified via email and will be required to sign and return via email/email link, a Declaration of Eligibility, liability/publicity release (except where prohibited by law) and assignment of rights (“Declaration Package”) within twenty-four (24) hours of notification attempt or potential Semi-Finalists may be disqualified and then one (1) of the Alternates may be contacted and they will be required to return the Declaration Package, time permitting. Return of Declaration Package as undeliverable, if any portion of the Declaration Package is not received within the required time period or if a potential Semi-Finalist/Alternate is deemed ineligible, said potential Semi-Finalist/Alternate will be disqualified and another Alternate will be contacted as stated above, time permitting. Once an Alternate is confirmed (if necessary), they will be deemed a confirmed Semi-Finalist.</p>
+  <p>In addition, confirmed Semi-Finalists will be notified by Sponsor separately from the Declaration Package via email with details on how to submit a Semi-Finalist submission (the “Round 2 Semi-Finalist Submission”). Round 2 Semi-Finalist Submission instructions will be provided via email on or about August 4, 2023. Round 2 Semi-Finalist Submission must be submitted by 9:00 p.m. ET on August 15, 2023 to be eligible. Round 2 Semi-Finalist Submissions must be submitted as Google Slides and may not contain more than fifteen (15) Google Slides. Round 2 Semi-Finalist Submissions must be derived from the Semi-Finalist's Round 1 Entry. All details of the Round 2 Semi-Finalist Submissions will be in Sponsor's sole discretion and will be explained to Semi-Finalist Entrants prior to the Semi-Finalist Presentation (defined below).</p>
+  <p>All Semi-Finalists will each receive a Semi-Finalist Prize, as described below and subject to verification.</p>
+<p>Semi-Finalists will then have the opportunity to present their Round 2 Semi-Finalist Submissions in person in front of the Judges at a live event hosted at the Samsung Galaxy Experience Space in Herald Towers at 50 W. 34th St., New York, NY 10001, on August 17, 2023 beginning at approximately 10:00 a.m. ET (the “Semi-Finalist Presentation”). All travel expenses are responsibility of the Semi-Finalist but A/V equipment that may be necessary will be provided by Sponsor. Semi-Finalists (in the order determined by Sponsor) will have fifteen (15) minutes to conduct their Semi-Finalist Presentation in front of a Sponsor-selected panel of judges and a live audience. The Semi-Finalist Presentation will then be judged based on the following:</p>
+<ol className='custom-list list-inside text-left pl-4'>
+  <li>25%: Presentation quality - Visuals and delivery</li>
+  <li>25%: Strategy/Thinking - Did it deliver on the ask?</li>
+  <li>25%: Execution - Is it impactful and buzz worthy?</li>
+  <li>25%: Results - Is it measurable and why should this win? (collectively, the “Presentation Judging Criteria”).</li>
+</ol>
+
+<p>Three (3) Finalists will be determined by different Judges based on the Presentation Judging Criteria on or around 6:00 p.m. ET on August 17, 2023. Any tied Semi-Finalists will be rejudged by the judges based 100% on Semi-Finalist Presentation quality to determine the applicable Finalist.</p>
+<p>Finalists will then be required to re-present their Semi-Finalist Submissions in person for a final judging at a live event hosted at the Samsung Galaxy Experience Space in Herald Towers at 50 W. 34th St., New York, NY 10001, on August 18, 2023 beginning at approximately 1:00 p.m. ET (the “Finalist Presentation”). Each Finalist (in the order determined by Sponsor) will have fifteen (15) minutes to conduct their Finalist Presentation in front of a new Sponsor-selected panel of judges and a live audience. Finalist Presentations will then be judged based on the Presentation Judging Criteria to determine one (1) Grand Prize winner following the live event.</p>
+
+  <li>Execution (Use of Media, Impact, Creative): 33%</li>
+</ol>
+
     </main>
     </div>
   )
